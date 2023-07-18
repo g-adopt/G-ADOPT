d7f539042041ab3c7db6a5bfa9d84e5c37b87159
Sia Ghelichkhan <siavash.ghelichkhan@anu.edu.au>
2023-07-18 15:08:41 +1000

    modifying forward for testing solver params

--- demos/adjoint_2d_cylindrical/forward.py
+++ demos/adjoint_2d_cylindrical/forward.py
@@ -1,10 +1,28 @@
 from gadopt import *
+import numpy as np
 
-# Quadrature degree:
-dx = dx(degree=6)
+newton_stokes_solver_parameters = {
+    "snes_type": "newtonls",
+    "snes_linesearch_type": "l2",
+    "snes_max_it": 100,
+    "snes_atol": 1e-10,
+    "snes_rtol": 1e-5,
+    "snes_stol": 0,
+    # "snes_monitor": ':./newton.txt',
+    "ksp_type": "preonly",
+    "pc_tyoe": "lu",
+    "pc_factor_mat_solver_type": "mumps",
+    # "snes_view": none,
+    "snes_converged_reason": None,
+    "fiedsplit_0": {
+        "ksp_converged_reason": None,
+    },
+    "fiedsplit_1": {
+        "ksp_converged_reason": None,
+    }}
 
 # Set up geometry:
-rmin, rmax, ncells, nlayers = 1.22, 2.22, 128, 32
+rmin, rmax, ncells, nlayers = 1.22, 2.22, 512, 128
 rmax_earth = 6370  # Radius of Earth [km]
 rmin_earth = rmax_earth - 2900  # Radius of CMB [km]
 r_410_earth = rmax_earth - 410  # 410 radius [km]
@@ -25,12 +43,6 @@ Z = MixedFunctionSpace([V, W])  # Mixed function space.
 # Test functions and functions to hold solutions:
 z = Function(Z)  # a field over the mixed function space Z.
 u, p = split(z)  # Returns symbolic UFL expression for u and p
-
-# Output function space information:
-log("Number of Velocity DOF:", V.dim())
-log("Number of Pressure DOF:", W.dim())
-log("Number of Velocity and Pressure DOF:", V.dim() + W.dim())
-log("Number of Temperature DOF:", Q.dim())
 
 # Set up temperature field and initialise:
 X = SpatialCoordinate(mesh)
@@ -75,18 +87,26 @@ mu_plast = 0.1 + (sigma_y / epsii)
 mu_eff = 2 * (mu_lin * mu_plast)/(mu_lin + mu_plast)
 mu = conditional(mu_eff > 0.4, mu_eff, 0.4)
 
+# viscosity function
+mu_function = Function(W, name="Viscosity")
+
+# radial average temperature function
+Taverage = Function(W, name="AverageTemperature")
+
+# Calculate the layer average of the initial state
+averager = LayerAveraging(
+    mesh,
+    np.linspace(rmin, rmax, nlayers*2),
+    cartesian=False,
+    quad_degree=6)
+
 # Nullspaces and near-nullspaces:
 Z_nullspace = create_stokes_nullspace(Z, closed=True, rotational=True)
 Z_near_nullspace = create_stokes_nullspace(Z, closed=False, rotational=True, translations=[0, 1])
 
-# Write output files in VTK format:
-u, p = z.subfunctions  # Do this first to extract individual velocity and pressure fields.
-# Next rename for output:
-u.rename("Velocity")
-p.rename("Pressure")
 # Create output file and select output_frequency:
-output_file = File("output.pvd")
-dump_period = 1
+output_file = File("vtk-files/output.pvd")
+dump_period = 10
 # Frequency of checkpoint files:
 checkpoint_period = dump_period * 4
 # Open file for logging diagnostic output:
@@ -107,31 +127,45 @@ stokes_solver = StokesSolver(
     bcs=stokes_bcs,
     cartesian=False,
     nullspace=Z_nullspace, transpose_nullspace=Z_nullspace,
-    near_nullspace=Z_near_nullspace)
+    near_nullspace=Z_near_nullspace,
+    solver_parameters=newton_stokes_solver_parameters)
+
+# computing the average
+averager.extrapolate_layer_average(
+    Taverage,
+    averager.get_layer_average(T)
+)
 
 checkpoint_file = CheckpointFile("Checkpoint_State.h5", "w")
 checkpoint_file.save_mesh(mesh)
+checkpoint_file.save_function(Taverage, name="AverageTemperature", idx=0)
+
+# splitting and renaming the functions for visualisation
+u_, p_ = z.subfunctions
+u_.rename("Velocity")
+p_.rename("Pressure")
+
 
 # Now perform the time loop:
 for timestep in range(0, max_timesteps):
 
     # Write output:
-    if timestep % dump_period == 0:
-        output_file.write(u, p, T)
+    if (timestep % dump_period == 0 or
+            timestep == max_timesteps - 1):
+        mu_function.interpolate(mu)
+        output_file.write(u_, p_, T)
 
     # Solve Stokes sytem:
     stokes_solver.solve()
+
+    # store velocity for return
+    checkpoint_file.save_function(u_, idx=timestep)
 
     # Temperature system:
     energy_solver.solve()
 
     # Checkpointing:
-    if timestep % checkpoint_period == 0:
+    if timestep == max_timesteps - 1:
         checkpoint_file.save_function(T, name="Temperature", idx=timestep)
-        checkpoint_file.save_function(z, name="Stokes", idx=timestep)
 
 checkpoint_file.close()
-
-with CheckpointFile("Final_State.h5", "w") as final_checkpoint:
-    final_checkpoint.save_mesh(mesh)
-    final_checkpoint.save_function(T, name="Temperature")
